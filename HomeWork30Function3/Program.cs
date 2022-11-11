ef427209aa6a35fe1eb3337885d41e69fa256495
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-11-11 19:37:33 +0300

    HomeWork30Function3

---
+++ HomeWork30Function3/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int searchedNumber = 10;
+
+        Console.Write("Введите число: ");
+        string userInput = Console.ReadLine();
+
+        FindNumber(userInput, searchedNumber);
+    }
+
+    static void FindNumber(string userInput, int searchedNumber)
+    {
+        bool isWorking = true;
+        int number;
+
+        while (isWorking)
+        {
+            int.TryParse(userInput, out number);
+
+            if (number != searchedNumber)
+            {
+                Console.Write("Конвертация не удалась или введено неверное чило. Введите число снова: ");
+                userInput = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Конвертация удалась. Ваше число - " + number);
+                isWorking = false;
+            }
+        }
+    }
+}
