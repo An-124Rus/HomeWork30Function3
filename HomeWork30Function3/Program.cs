1c28a1309ac0dc30924bf29fb08e2a9f26a145a6
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-11-13 20:09:15 +0300

    Add second method

--- HomeWork30Function3/Program.cs
+++ HomeWork30Function3/Program.cs
@@ -4,7 +4,11 @@
     {
         int number = ReturnNumber();
 
-        Console.WriteLine("Конвертация удалась. Ваше число - " + number);
+        Console.WriteLine("Конвертация 1 удалась. Ваше число - " + number);
+
+        number = ReturnNumber2();
+
+        Console.WriteLine("Конвертация 2 удалась. Ваше число - " + number);
     }
 
     static int ReturnNumber()
@@ -17,6 +21,21 @@
             Console.Write("Введите целое число: ");
             userInput = Console.ReadLine();
         } while (int.TryParse(userInput, out number) == false);
+
+        return number;
+    }
+    
+    static int ReturnNumber2()
+    {
+        Console.Write("Введите число: ");
+        string userInput = Console.ReadLine();
+        int number;
+
+        while (int.TryParse(userInput, out number) == false)
+        {
+                Console.Write("Конвертация не удалась или введено неверное чиcло. Введите число снова: ");
+                userInput = Console.ReadLine();
+        }
 
         return number;
     }
